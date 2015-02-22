8be6cb18d0103100ac9cb25b169558aa6f7f6ffa
Samuel De Backer <sdebacker@gmail.com>
2015-02-22 01:03:02 +0100

    migration

--- src/database/migrations/2013_09_03_084148_create_menulinks_tables.php
+++ src/database/migrations/2013_09_03_084148_create_menulinks_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenusTables extends Migration
+class CreateMenulinksTables extends Migration
 {
     /**
      * Run the migrations.
